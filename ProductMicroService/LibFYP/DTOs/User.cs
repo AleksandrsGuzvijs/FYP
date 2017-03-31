e10c4146a3a769e971d903396fb9059815b64984
superharek <superharek@yandex.ru>
2017-03-31 09:37:07 +0100

    Added Product Microservice.

---
+++ ProductMicroService/LibFYP/DTOs/User.cs
@@ -0,0 +1,14 @@
+﻿namespace LibFYP.DTOs
+{
+    /// <summary>
+    /// Class User.
+    /// </summary>
+    /// <seealso cref="LibFYP.Dto" />
+    public class User : Dto
+    {
+        public virtual string Username { get; set; }
+        public virtual string Password { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+    }
+}
---
+++ ProductMicroService/ProductMicroService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProductMicroService.WebApiApplication" Language="C#" %>

95c9d2eb0a8a2ae0256362308ccda5ce656755a9
n3033811 <n3033811@live.tees.ac.uk>
2017-05-05 14:07:29 +0100

    ..

--- WebApi/WebApi/Controllers/SelectionboxesController.cs
+++ WebApi/WebApi/Controllers/SelectionboxesController.cs
@@ -6,28 +6,28 @@ using System.Threading.Tasks;
 
 namespace WebApi.Controllers
 {
-    [Route("api/[controller]")]
-    public class SelectionboxesController : Controller
-    {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
+    //[Route("api/[controller]")]
+    //public class SelectionboxesController : Controller
+    //{
+    //    // GET: api/values
+    //    [HttpGet]
+    //    public IEnumerable<string> Get()
+    //    {
             
-        }
+    //    }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
+    //    // GET api/values/5
+    //    [HttpGet("{id}")]
+    //    public string Get(int id)
+    //    {
             
-        }
+    //    }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
+    //    // POST api/values
+    //    [HttpPost]
+    //    public void Post([FromBody]string value)
+    //    {
+    //    }
         
-    }
+    //}
 }

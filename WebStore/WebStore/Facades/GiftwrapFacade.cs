cb13d22f0cd85a4f4a9c712304f6fb33014bdebd
n3033811 <n3033811@live.tees.ac.uk>
2017-05-05 14:01:31 +0100

    .

---
+++ WebStore/WebStore/Facades/GiftwrapFacade.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using LibFYP.DTOs;
+
+namespace WebStore.Facades
+{
+    public class GiftwrapFacade
+    {
+        private readonly HttpClient _client;
+
+        public GiftwrapFacade()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new System.Uri("");
+            _client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+        }
+
+        [HttpGet]
+        public async Task<IQueryable<Wrapping>> GetGiftwraps(string rangeName = null, string typeName = null, int? typeId = null, int? rangeId = null, double? minPrice = 0, double? maxPrice = double.MaxValue)
+        {
+            try
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+                response = await _client.GetAsync(_baseUrl + "giftwraps" + "/").WithQueryString("rangeName=" + rangeName + "&typeName" + typeName + "&typeId" + typeId + "&rangeId" + rangeId + "&minPrice=" + MinPrice + "&maxPrice=" + MaxPrice).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    IQueryable<Wrapping> data = response.Content.ReadAsAsync<IEnumerable<Wrapping>>().Result;
+
+                    data = data.Where(d => d.Price >= minPrice);
+                    data = data.Where(d => d.Price <= (maxPrice == 0 ? Double.MaxValue : maxPrice));
+                    if (rangeName != null) data = data.Where(d => d.RangeName == rangeName);
+                    if (typeName != null) data = data.Where(d => d.TypeName == typeName);
+                    if (typeId != null) data = data.Where(d => d.TypeId == typeId);
+                    if (rangeId != null) data = data.Where(d => d.RangeId == rangeId);
+
+                    return data;
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                return Enumerable.Empty<LibAyycorn.Dtos.Giftbox>().AsQueryable();
+            }
+        }
+
+        [HttpGet]
+        public async Task<HttpResponseMessage> GetGiftwrap(int id)
+        {
+            try
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+                response = await _client.GetAsync(_baseUrl + "giftwraps" + "/" + id);
+                return response;
+            }
+
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ WebStore/WebStore/Facades/SelectionboxFacade.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using LibFYP.DTOs;
+
+namespace WebStore.Facades
+{
+    public class SelectionboxFacade
+    {
+        private readonly HttpClient _client;
+
+        public SelectionboxFacade()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new System.Uri("http://ayycornapi.azurewebsites.net/api/");
+            _client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+        }
+
+
+        public async Task<IEnumerable<Giftbox>> GetSelectionBoxes()
+        {
+            try
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+                response = await _client.GetAsync(_baseUrl + "selectionbox").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsAsync<IEnumerable<Giftbox>>().Result;
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+
+        public async Task<HttpResponseMessage> GetSelectionBox(int id)
+        {
+            try
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+                response = await _client.GetAsync(_baseUrl + "selectionbox/" + id);
+                return response;
+            }
+
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+
+        public async Task<HttpResponseMessage> PostSelectionBox(Giftbox giftbox)
+        {
+            try
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+
+                Giftbox giftbox = new Giftbox
+                {
+
+                };
+
+                response = await _client.PostAsJsonAsync(_baseUrl + "selectionbox/", giftbox).Result;
+            }
+
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+        }
+
+
+        public async Task<HttpResponseMessage> UpdateSelectionBox(Giftbox giftbox)
+        {
+
+        }
+
+
+        public async Task<HttpResponseMessage> RemoveSelectionBox(int id)
+        {
+            try
+            {
+                HttpResponseMessage response = new HttpResponseMessage();
+
+                response = await _client.PostAsJsonAsync(_baseUrl + "selectionbox/delete", +id).Result;
+            }
+
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
